685e8f1fc9ad8b1ebbfd6d3011beb56df9c2d35b
katiatzelepe <50484691+katiatzelepe@users.noreply.github.com>
2019-05-23 11:43:46 +0300

    Update CustomerService.cs

--- CustomerService.cs
+++ CustomerService.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace OrderManagment
+namespace  BasketManagment
 {
     public class CustomerService : ICustomerService
     {
